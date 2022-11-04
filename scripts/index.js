a5815e2b22829eca687205f762a036b393129bbd
PeachMood <vas140302@gmail.com>
2022-11-04 18:29:09 +0700

    feat: добавить стили для валидации форм

--- scripts/index.js
+++ scripts/index.js
@@ -12,22 +12,16 @@ const nameTitle = profile.querySelector('.profile__name');
 const jobTitle = profile.querySelector('.profile__job');
 
 const profilePopup = document.querySelector('.popup_profile');
-const profilePopupContainer = profilePopup.querySelector('.popup__container');
-const profilePopupButtonClose = profilePopup.querySelector('.popup__close');
 const profileForm = profilePopup.querySelector('.form');
 const nameInput = profileForm.querySelector('.form__input_name');
 const jobInput = profileForm.querySelector('.form__input_job');
 
 const cardPopup = document.querySelector('.popup_card');
-const cardPopupContainer = cardPopup.querySelector('.popup__container');
-const cardPopupButtonClose = cardPopup.querySelector('.popup__close');
 const cardForm = cardPopup.querySelector('.form');
 const placeInput = cardForm.querySelector('.form__input_place');
 const linkInput = cardForm.querySelector('.form__input_link');
 
 const figurePopup = document.querySelector('.popup_figure');
-const figurePopupContainer = figurePopup.querySelector('.popup__container');
-const figurePopupButtonClose = figurePopup.querySelector('.popup__close');
 const figureImage = figurePopup.querySelector('.figure__image');
 const figureText = figurePopup.querySelector('.figure__text');
 
@@ -63,13 +57,11 @@ function openPopup(popup) {
 function handleEditClick() {
   nameInput.value = nameTitle.textContent;
   jobInput.value = jobTitle.textContent;
-
   openPopup(profilePopup);
 }
 
 function handleAddClick() {
   cardForm.reset();
-
   openPopup(cardPopup);
 }
 
